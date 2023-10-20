f30384fd5468adf1ea0f66b8555c20a2e2025962
Morozyuk Daniil <morozyuk.d.p@gmail.com>
2023-10-20 08:02:19 +0000

    Flaskified the program

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify, json
+from .main import assistant
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def assistant_route():
+    rq = json.loads(request.get_data(as_text=True))['question']
+    rs = assistant(rq)
+    return jsonify(response=rs)
